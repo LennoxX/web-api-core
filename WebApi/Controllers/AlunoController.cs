622c5330d803efa0efc80e5b90165359e97d8089
Lucas Raphael Fernandes Ferreira <lucasraphael.fernandes@hotmail.com>
2020-11-17 17:22:08 -0300

    Criando base

--- WebApi/Controllers/AlunoController.cs
+++ WebApi/Controllers/AlunoController.cs
@@ -39,6 +39,61 @@ namespace WebApi.Controllers
             return BadRequest("Ocorreu um Erro não esperado!");
         }
 
+        [HttpPut]
+        public async Task<IActionResult> Put(Aluno resource)
+        {
+            try
+            {
+                var aluno = await _repo.GetAlunoAsyncById(resource.Id);
+                if (aluno == null)
+                {
+                    return NotFound("Recurso não encontrado");
+                }
+                else
+                {
+                    _repo.Update(resource);
+                    if (await _repo.SaveChangesAsync())
+                    {
+                        return Ok(resource);
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            return BadRequest("Ocorreu um Erro não esperado!");
+        }
+
+        [HttpDelete("{AlunoId}")]
+        public async Task<IActionResult> Delete(int alunoId)
+        {
+            try
+            {
+                var aluno = await _repo.GetAlunoAsyncById(alunoId);
+                if (aluno == null)
+                {
+                    return NotFound("Recurso não encontrado");
+                }
+                else
+                {
+                    _repo.Delete(aluno);
+                    if (await _repo.SaveChangesAsync())
+                    {
+                        return Ok(aluno);
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            return BadRequest("Ocorreu um Erro não esperado!");
+        }
+
+
         [HttpGet]
         public async Task<IActionResult> Get()
         {
--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -21,10 +21,21 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(
+                   name: "AllowOrigin",
+                   builder =>
+                   {
+                       builder.AllowAnyOrigin()
+                      .AllowAnyMethod()
+                      .AllowAnyHeader();
+                   });
+            });
             services.AddDbContext<DataContext>(
                 x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")
             ));
-
+            services.AddControllers().AddNewtonsoftJson();
             services.AddControllers();
             services.AddScoped<IRepository, Repository>();
             services.AddSwaggerGen(c =>
@@ -36,6 +47,7 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors("AllowOrigin");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
